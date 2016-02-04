5d0efab9528bb7c3dbf652970ddc015542006ebf
djbb7 <daniel.bruzual@gmail.com>
2016-02-04 22:07:25 +0100

    Change tabs for spaces

--- create_schema.sql
+++ create_schema.sql
@@ -12,47 +12,48 @@ CREATE SEQUENCE goal_id_seq;
 CREATE SEQUENCE run_history_id_seq;
 
 CREATE TABLE PERSON (
-	id INT PRIMARY KEY NOT NULL DEFAULT nextval('person_id_seq'),
-	firstname VARCHAR(30),
-	lastname VARCHAR(30),
-	email VARCHAR(50),
-	strava_access_token VARCHAR(80),
-	strava_code VARCHAR(30),
-	telegram_user_id INT,
-	telegram_chat_id INT,
-	slack_user_id VARCHAR(16)
+    id INT PRIMARY KEY NOT NULL DEFAULT nextval('person_id_seq'),
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
+    email VARCHAR(50),
+    strava_access_token VARCHAR(80),
+    strava_code VARCHAR(30),
+    telegram_user_id INT,
+    telegram_chat_id INT,
+    slack_user_id VARCHAR(16)
 );
 
 CREATE TABLE GOAL (
-	id INT PRIMARY KEY NOT NULL DEFAULT nextval('goal_id_seq'),
-	person_id INT NOT NULL,
-	target_value DECIMAL(5,2),
-	creation_date TIMESTAMP,
-	goal_measure_type VARCHAR(15),
-	goal_period INT
+    id INT PRIMARY KEY NOT NULL DEFAULT nextval('goal_id_seq'),
+    person_id INT NOT NULL,
+    target_value DECIMAL(10,2),
+    creation_date TIMESTAMP,
+    goal_measure_type VARCHAR(15),
+    goal_period INT
 ); 
 
 CREATE TABLE GOAL_MEASURE_TYPE(
-	id VARCHAR(15) PRIMARY KEY,
-	units VARCHAR(15),
-	name VARCHAR(15) UNIQUE
+    id VARCHAR(15) PRIMARY KEY,
+    units VARCHAR(15),
+    name VARCHAR(15) UNIQUE
 );
 
 CREATE TABLE GOAL_PERIOD(
-	days_length INT PRIMARY KEY NOT NULL,
-	name VARCHAR(20) UNIQUE
+    days_length INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20) UNIQUE
 );
 
 CREATE TABLE RUN_HISTORY(
-	id INT PRIMARY KEY NOT NULL DEFAULT nextval('run_history_id_seq'),
-	person_id INT NOT NULL,
-	distance DECIMAL(12,4),
-	calories DECIMAL(12,4),
-	startdate TIMESTAMP,
-	moving_time DECIMAL(12,4),
-	elevation_gain DECIMAL(12,4),
-	max_speed DECIMAL(8,4),
-	avg_speed DECIMAL(8,4)
+    id INT PRIMARY KEY NOT NULL DEFAULT nextval('run_history_id_seq'),
+    person_id INT NOT NULL,
+    distance DECIMAL(12,4),
+    calories DECIMAL(12,4),
+    startdate TIMESTAMP,
+    moving_time DECIMAL(12,4),
+    elevation_gain DECIMAL(12,4),
+    max_speed DECIMAL(8,4),
+    avg_speed DECIMAL(8,4),
+    steps INT
 );
 
 
--- insert.sql
+++ insert.sql
@@ -2,15 +2,18 @@ INSERT INTO PERSON (id, firstname, lastname, email, slack_user_id)
 VALUES  (1, 'Daniel', 'Bruzual', 'danielbruzual@gmail.com', 'U43F341'), 
         (3, 'Damiano', 'Fossa', 'damianofossa@gmail.com', 'UB23312'), 
         (5, 'Ruslano', 'Pallinco', 'ruslano.pallinco@gmail.com', 'UA22049');
+SELECT setval('person_id_seq', 6, true);
 
 INSERT INTO GOAL_PERIOD (days_length, name)
-VALUES  (1, 'daily'), (7, 'weekly'), (30, 'monthly');
+VALUES  (1, 'daily'), 
+        (7, 'weekly'), 
+        (30, 'monthly');
 
 INSERT INTO GOAL_MEASURE_TYPE (id, units, name)
-VALUES  (2, 'meters','distance'),
-        (4, 'minutes','time'),
-        (6, 'calories','calories'),
-        (8, 'steps', 'steps');
+VALUES  ('distance', 'meters','distance ran'),
+        ('run_time', 'minutes','time moving'),
+        ('calories', 'calories','calories burnt'),
+        ('steps', 'steps', 'steps walked');
 
 INSERT INTO GOAL (person_id, target_value, goal_measure_type, goal_period)
 VALUES  (1,  2000, 2, 1),
